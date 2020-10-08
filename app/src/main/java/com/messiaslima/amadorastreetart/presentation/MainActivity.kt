934df41fcb4833682d8bd366cebe6010d2cd0b7a
Messias Junior <messiaslima.03@gmail.com>
2020-10-08 22:18:41 +0100

    chore: application bootstrap

---
+++ app/src/main/java/com/messiaslima/amadorastreetart/presentation/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.messiaslima.amadorastreetart.presentation
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.messiaslima.amadorastreetart.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
